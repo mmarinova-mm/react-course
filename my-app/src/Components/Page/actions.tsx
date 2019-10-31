e1b9279a39bc5ef8de3e8c57074d3d805458a3c6
Marina Marinova <marina.marinova@mentormate.com>
2019-10-31 11:31:56 +0200

    react wip

---
+++ my-app/src/Components/Page/actions.tsx
@@ -0,0 +1,17 @@
+export const INCREMENT_VOTES = 'INCREMENT_VOTES';
+export const DECREMENT_VOTES = 'DECREMENT_VOTES';
+export const SORT_PIZZAS = 'SORT_PIZZAS';
+export const INDEX_PIZZAS = 'INDEX_PIZZASS';
+
+export function decrementPizzaVotes(pizzaId: number) {
+    return {type: DECREMENT_VOTES, id: pizzaId};
+}
+export function sortPizzas() {
+    return {type: SORT_PIZZAS};
+}
+export function indexPizzas() {
+    return {type: INDEX_PIZZAS};
+}
+export function incrementPizzaVotes(pizzaId: number) {
+    return {type: INCREMENT_VOTES, id: pizzaId};
+}
--- my-app/src/Components/Page/index.tsx
+++ my-app/src/Components/Page/index.tsx
@@ -1,6 +1,32 @@
 import React, {useState} from 'react';
 import IPizza from "./PizzaList/interfaces/IPizza";
 import PizzaList from "./PizzaList";
+import {DECREMENT_VOTES, INCREMENT_VOTES, INDEX_PIZZAS, SORT_PIZZAS} from "./actions";
+
+const initialState = {
+    pizzas: [],
+};
+
+function pizzaActions(state: any = initialState, action: any) {
+    switch (action.type) {
+        case INCREMENT_VOTES:
+            return Object.assign({}, state, {
+                pizzas: state.pizzas.map(p => p.id === action.pizzaId ? {...p, votesCount: p.votesCount--} : p)
+            });
+        case DECREMENT_VOTES:
+            return Object.assign({}, state, {
+                pizzas: state.pizzas.map(p => p.id === action.pizzaId ? {...p, votesCount: p.votesCount++} : p)
+            });
+        case SORT_PIZZAS:
+            return Object.assign({}, state, {
+                pizzas: state.pizzas.sort((a: IPizza, b: IPizza) => (b.votesCount - a.votesCount))
+            });
+        case INDEX_PIZZAS:
+            return state.pizzas;
+        default:
+            return state;
+    }
+}
 
 interface IPageProps {
     initData: Array<IPizza>,
@@ -8,23 +34,33 @@ interface IPageProps {
 
 export default function Page({initData}: IPageProps) {
     const [votes, setVotes] = useState<number[]>([]);
-    const [pizzas, setPosts] = useState<IPizza[]>(initData);
+    const [pizzas, setPizzas] = useState<IPizza[]>(initData);
 
+    // const upvote = (pizza: IPizza) => {
+    //     if (votes.includes(pizza.id)) {
+    //         setVotes(votes.filter(vote => vote !== pizza.id));
+    //         setPizzas(pizzas.map(p => p.id === pizza.id ? {...p, votesCount: p.votesCount--} : p));
+    //     } else {
+
+    //         setVotes([...votes, pizza.id]);
+    //     }
+    //
+    //     setPizzas(pizzas.sort((a, b) => (b.votesCount - a.votesCount)));
+    // };    
     const upvote = (pizza: IPizza) => {
         if (votes.includes(pizza.id)) {
             setVotes(votes.filter(vote => vote !== pizza.id));
-            setPosts(pizzas.map(p => p.id === pizza.id ? {...p, votesCount: p.votesCount--} : p));
+            incrementPizzaVotes(pizza.id);
         } else {
-            setPosts(pizzas.map(p => p.id === pizza.id ? {...p, votesCount: p.votesCount++} : p));
+            decrementPizzaVotes(pizza.id);
             setVotes([...votes, pizza.id]);
         }
-
-        setPosts(pizzas.sort((a, b) => (b.votesCount - a.votesCount)));
+        sortPizzas();
     };
 
-    const sortPizzas = () => {
-        return pizzas.sort((a, b) => (b.votesCount - a.votesCount));
-    };
+    // const sortPizzas = () => {
+    //     return pizzas.sort((a, b) => (b.votesCount - a.votesCount));
+    // };
 
-    return <main><PizzaList pizzas={sortPizzas()} upvote={upvote}/></main>;
+    return <main><PizzaList pizzas={indexPizzas()} upvote={upvote}/></main>;
 }
