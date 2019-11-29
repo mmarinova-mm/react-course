8be4583a90a730017072d6499f504adc2ccdd21d
Marina Marinova <marina.marinova@mentormate.com>
2019-11-29 22:05:27 +0200

    Router works

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,16 +7,22 @@ import {composeWithDevTools} from "redux-devtools-extension";
 import {pizzaReducer, sortPizzas} from "./modules/pizzas";
 import {cartReducer} from "./modules/cart";
 import MainPage from "./pages/MainPage";
-import { save, load } from "redux-localstorage-simple"
+import {save, load} from "redux-localstorage-simple"
+import {BrowserRouter} from "react-router-dom";
 
 const createStoreWithMiddleware = compose(applyMiddleware(save()))(createStore);
-const store = createStoreWithMiddleware(combineReducers({pizzas: pizzaReducer, cart: cartReducer}), load({pizzas: pizzas}), composeWithDevTools());
+const store = createStoreWithMiddleware(combineReducers({
+    pizzas: pizzaReducer,
+    cart: cartReducer
+}), load({pizzas: pizzas}), composeWithDevTools());
 
 export default function App() {
     store.dispatch(sortPizzas());
     return (
-        <Provider store={store}>
-            <MainPage/>
-        </Provider>
+        <div>
+            <Provider store={store}>
+                <MainPage/>
+            </Provider>
+        </div>
     );
 }
--- my-app/src/pages/MainPage/index.tsx
+++ my-app/src/pages/MainPage/index.tsx
@@ -4,6 +4,8 @@ import {decrementPizzaVotes, incrementPizzaVotes, IPizza, sortPizzas} from "../.
 import PizzaList from '../../components/PizzaList';
 import Cart from '../../components/Cart';
 import CheckoutForm from "../../components/CheckoutForm";
+import {Route, Switch} from 'react-router';
+import {BrowserRouter, Link} from 'react-router-dom';
 
 export default function MainPage() {
     const dispatch = useDispatch();
@@ -23,9 +25,28 @@ export default function MainPage() {
         dispatch(sortPizzas());
     };
 
-    return <main>
-        <PizzaList pizzas={pizzas} upvote={upvote}/>
-        <Cart/>
-        <CheckoutForm/>
-    </main>;
+    return <BrowserRouter>
+            <header>
+                <nav>
+                    <ul>
+                        <li><Link to="/">Pizzas</Link></li>
+                        <li><Link to="/cart">Cart</Link></li>
+                        <li><Link to="/checkout">Checkout</Link></li>
+                    </ul>
+                </nav>
+            </header>
+            <main>
+                <Switch>
+                    <Route exact path="/">
+                        <PizzaList pizzas={pizzas} upvote={upvote}/>
+                    </Route>
+                    <Route path="/cart">
+                        <Cart/>
+                    </Route>
+                    <Route path="/checkout">
+                        <CheckoutForm/>
+                    </Route>
+                </Switch>
+            </main>
+        </BrowserRouter>;
 }
